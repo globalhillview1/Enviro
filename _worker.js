6aaf3766f9d1c157bafe35229942ae3a9ca3dca5
globalhillview1 <globalhillview1@gmail.com>
2025-10-31 22:31:53 +0530

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,6 +18,18 @@ function corsHeaders(origin) {
   };
 }
 
+// at the very top of fetch():
+const url = new URL(request.url);
+if (url.pathname === '/api' && url.searchParams.get('op') === 'ping') {
+  return new Response(JSON.stringify({ ok: true, pong: true }), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json',
+      'access-control-allow-origin': url.origin || '*'
+    }
+  });
+}
+
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
